c99f5a22865516c4edafe6594746cca3a404c028
Ernesto Sánchez <esanchez@ucasal.edu.ar>
2026-08-09 23:47:53 -0300

    Add TRUNCATE for link_health_checkpoints table

--- scripts/clean_link_health.sql
+++ scripts/clean_link_health.sql
@@ -2,3 +2,4 @@ TRUNCATE TABLE link_health_metrics CASCADE;
 TRUNCATE TABLE link_health_ground_truth CASCADE;
 TRUNCATE TABLE link_health_features CASCADE;
 TRUNCATE TABLE link_health_metrics_enriched CASCADE;
+TRUNCATE TABLE link_health_checkpoints CASCADE;
