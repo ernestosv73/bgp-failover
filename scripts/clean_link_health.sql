7e77464b4fe85a452474deee31ef8d04140ff4b7
Ernesto Sánchez <esanchez@ucasal.edu.ar>
2026-08-09 23:46:53 -0300

    Update clean_link_health.sql

--- scripts/clean_link_health.sql
+++ scripts/clean_link_health.sql
@@ -1,3 +1,4 @@
 TRUNCATE TABLE link_health_metrics CASCADE;
 TRUNCATE TABLE link_health_ground_truth CASCADE;
 TRUNCATE TABLE link_health_features CASCADE;
+TRUNCATE TABLE link_health_metrics_enriched CASCADE;
